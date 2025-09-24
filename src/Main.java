7c45c2933ac363723d189739c1764715d941ba3a
Severetoya <nikita.sevryukov@gmail.com>
2025-09-24 19:10:30 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,125 +1,59 @@
 public class Main {
     public static void main(String[] args) {
-        // Задание 1
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
+        printThreeWords(); // Задание 1
 
-        // Задание 2
         int a = 10;
         int b = -5;
-        int sum = a + b;
-        if (sum >= 0) {
-            System.out.println("Сумма положительная");
-        } else {
-            System.out.println("Сумма отрицательная");
-        }
+        checkSumSign(a, b); // Задание 2
 
-        // Задание 3
         int value = 46;
-        if (value <= 0) {
-            System.out.println("Красный");
-        } else if (value > 0 && value <= 100) {
-            System.out.println("Желтый");
-        } else {
-            System.out.println("Зеленый");
-        }
+        printColor(value); // Задание 3
 
-        // Задание 4
         int a1 = 15;
         int b1 = 10;
-        if (a1 >= b1) {
-            System.out.println("a >= b");
-        } else {
-            System.out.println("a < b");
-        }
+        compareNumbers(a1, b1); // Задание 4
 
-        // Задание 5
         int num1 = 5;
         int num2 = 15;
-        int sum1 = num1 + num2;
-        if (sum1 >= 10 && sum1 <= 20) {
-            System.out.println("true");
-        } else {
-            System.out.println("false");
-        }
+        boolean sumBetween10And20 = isSumBetween10And20(num1, num2);
+        System.out.println(sumBetween10And20); // Задание 5
 
-        // Задание 6
         int number = 10;
-        if (number >= 0) {
-            System.out.println("Положительное");
-        } else {
-            System.out.println("Отрицательное");
-        }
+        printNumberSign(number); // Задание 6
 
-        // Задание 7
-        int number1 = 28;
-        if (number1 < 0) {
-            System.out.println("true");
-        } else {
-            System.out.println("false");
-        }
+        int number1 = -28;
+        boolean isNumNegative = isNegative(number1);
+        System.out.println(isNumNegative); // Задание 7
 
-        // Задание 8
         String str = "Why?";
         int times = 3;
-        for (int i = 0; i < times; i++) {
-            System.out.println(str);
-        }
+        printStringMultipleTimes(str, times); // Задание 8
 
-        // Задание 9
         int year = 2025;
-        if ((year % 400 == 0) || (year % 100 != 0 && year % 4 == 0)) {
-            System.out.println("true");
-        } else {
-            System.out.println("false");
-        }
+        boolean isYearLeap = isLeapYear(year);
+        System.out.println(isYearLeap); // Задание 9
 
-        // Задание 10
         int[] binaryArray = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-        for (int i = 0; i < binaryArray.length; i++) {
-            if (binaryArray[i] == 0) {
-                binaryArray[i] = 1;
-            } else {
-                binaryArray[i] = 0;
-            }
-        }
+        invertBinaryArray(binaryArray); // Задание 10
         for (int i = 0; i < binaryArray.length; i++) {
             System.out.print(binaryArray[i] + " ");
         }
         System.out.println();
 
-        // Задание 11
-        int[] emptyArray = new int[100];
-        for (int i = 0; i < emptyArray.length; i++) {
-            emptyArray[i] = i + 1;
-        }
+        int[] emptyArray = createAndFillArray(100); // Задание 11
         for (int i = 0; i < emptyArray.length; i++) {
             System.out.print(emptyArray[i] + " ");
         }
         System.out.println();
 
-        // Задание 12
         int[] arrayForTask12 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        for (int i = 0; i < arrayForTask12.length; i++) {
-            if (arrayForTask12[i] < 6) {
-                arrayForTask12[i] *= 2;
-            }
-        }
+        multiplyIfLessThan6(arrayForTask12); // Задание 12
         for (int i = 0; i < arrayForTask12.length; i++) {
             System.out.print(arrayForTask12[i] + " ");
         }
         System.out.println();
 
-        // Задание 13
-        int[][] squareArray = new int[5][5];
-        for (int i = 0; i < squareArray.length; i++) {
-            for (int j = 0; j < squareArray[i].length; j++) {
-                if (i == j) {
-                    squareArray[i][j] = 1;
-                }
-            }
-        }
+        int[][] squareArray = createAndFillDiagonalArray(5); // Задание 13
         for (int i = 0; i < squareArray.length; i++) {
             for (int j = 0; j < squareArray[i].length; j++) {
                 System.out.print(squareArray[i][j] + " ");
@@ -127,15 +61,149 @@ public class Main {
             System.out.println();
         }
 
-        // Задание 14
         int len = 10;
         int initialValue = 7;
-        int[] resultArray = new int[len];
-        for (int i = 0; i < len; i++) {
-            resultArray[i] = initialValue;
-        }
+        int[] resultArray = createArray(len, initialValue); // Задание 14
         for (int i = 0; i < resultArray.length; i++) {
             System.out.print(resultArray[i] + " ");
         }
+    }
+
+    // Задание 1
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    // Задание 2
+    public static void checkSumSign(int a, int b) {
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    // Задание 3
+    public static void printColor(int value) {
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    // Задание 4
+    public static void compareNumbers(int a, int b) {
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
+    // Задание 5
+    public static boolean isSumBetween10And20(int a, int b) {
+        int sum = a + b;
+        if (sum >= 10 && sum <= 20) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Задание 6
+    public static void printNumberSign(int num) {
+        if (num >= 0) {
+            System.out.println("Положительное");
+        } else {
+            System.out.println("Отрицательное");
+        }
+    }
+
+    // Задание 7
+    public static boolean isNegative(int num) {
+        if (num < 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Задание 8
+    public static void printStringMultipleTimes(String str, int times) {
+        for (int i = 0; i < times; i++) {
+            System.out.println(str);
+        }
+    }
+
+    // Задание 9
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else if (year % 4 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Задание 10
+    public static void invertBinaryArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                array[i] = 1;
+            } else {
+                array[i] = 0;
+            }
+        }
+    }
+
+    // Задание 11
+    public static int[] createAndFillArray(int length) {
+        int[] array = new int[length];
+        for (int i = 0; i < length; i++) {
+            array[i] = i + 1;
+        }
+        return array;
+    }
+
+    // Задание 12
+    public static void multiplyIfLessThan6(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 6) {
+                array[i] = array[i] * 2;
+            }
+        }
+    }
+
+    // Задание 13
+    public static int[][] createAndFillDiagonalArray(int size) {
+        int[][] array = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == j) {
+                    array[i][j] = 1;
+                } else {
+                    array[i][j] = 0;
+                }
+            }
+        }
+        return array;
+    }
+
+    // Задание 14
+    public static int[] createArray(int len, int initialValue) {
+        int[] array = new int[len];
+        for (int i = 0; i < len; i++) {
+            array[i] = initialValue;
+        }
+        return array;
     }
 }
